940ebc3cc603399ec0d0d8f38bdbeb539ef9cb97
Hooks7 <youremail@example.com>
2019-08-23 00:46:40 +0800

    素材列表获取数据

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,8 +3,14 @@
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
-    <el-tabs  v-model="tabPosition">
-      <el-tab-pane label="全部图片" name="first"></el-tab-pane>
+    <el-tabs v-model="tabPosition">
+      <el-tab-pane label="全部图片" name="first">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
       <el-tab-pane label="收藏图片" name="second"></el-tab-pane>
     </el-tabs>
   </el-card>
@@ -14,11 +20,42 @@
 export default {
   data () {
     return {
-      tabPosition: 'first'
+      tabPosition: 'first',
+      list: []
     }
+  },
+  methods: {
+    //   获取图片列表
+    getMaterial () {
+      this.$http({
+        url: 'user/images',
+        params: { collect: 'false' }
+      }).then(result => {
+        // console.log(result.data)
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.img-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 200px;
+    margin: 20px 10px;
+    position: relative;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
