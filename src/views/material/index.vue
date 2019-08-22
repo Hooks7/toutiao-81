7005aac6d4f0b556764fe4e22ee88069bc3192c6
Hooks7 <youremail@example.com>
2019-08-23 00:18:51 +0800

    素材列表页面结构

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,13 +1,24 @@
 <template>
-<div>素材管理</div>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs  v-model="tabPosition">
+      <el-tab-pane label="全部图片" name="first"></el-tab-pane>
+      <el-tab-pane label="收藏图片" name="second"></el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tabPosition: 'first'
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
