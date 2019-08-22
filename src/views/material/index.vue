cd07338aa876d2e2005b1eede2723027b44b8fb7
Hooks7 <youremail@example.com>
2019-08-23 01:32:09 +0800

    素材分页1

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,13 +16,24 @@
         </div>
       </el-tab-pane>
       <el-tab-pane label="收藏图片" name="collect">
-          <div class="img-list">
-            <el-card class="img-card" v-for="item in list" :key="item.id">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
-            </el-card>
+          </el-card>
         </div>
       </el-tab-pane>
     </el-tabs>
+    <!-- 分页 -->
+    <el-row type="flex" justify="center" style="margin:20px 0;">
+      <el-pagination
+        :page-size="page.pageSize"
+        :total="page.total"
+        :current-page="page.currentpage"
+        @current-change="changePage"
+        background
+        layout="prev, pager, next"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -30,22 +41,37 @@
 export default {
   data () {
     return {
+      page: {
+        currentPage: 1, // 当前页数
+        pageSize: 15, // 每页条数
+        total: 0
+      },
       tabPosition: 'all',
       list: []
     }
   },
   methods: {
+    // 分页事件
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     changeTab () {
       this.getMaterial()
     },
     //   获取图片列表
     getMaterial () {
+      let pageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
       this.$http({
         url: 'user/images',
-        params: { collect: this.tabPosition === 'collect' }
+        params: { collect: this.tabPosition === 'collect', ...pageParams }
       }).then(result => {
         // console.log(result.data)
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
