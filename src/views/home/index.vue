28aa668e472f5fa917b72c15efc55da817870faf
Hooks7 <youremail@example.com>
2019-08-27 00:01:49 +0800

    左侧导航栏伸缩

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
   <!-- 外层容器 -->
   <el-container>
     <!-- 左侧布局 -->
-    <el-aside style="width:200px; min-height:100vh; background-color:#323745">
+    <el-aside :style="{width}"  style="min-height:100vh; background-color:#323745">
       <layout-aside></layout-aside>
     </el-aside>
     <!-- 右侧布局 -->
@@ -12,14 +12,25 @@
        <layout-header></layout-header>
       </el-header>
       <el-main style="padding:0">
-<router-view></router-view>
+        <router-view></router-view>
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
+import eventBus from '../../utils/eventBus'
 export default {
+  data () {
+    return {
+      width: '200px'
+    }
+  },
+  created () {
+    eventBus.$on('collopseOrClose', () => {
+      this.width = this.width === '200px' ? '60px' : '200px'
+    })
+  }
 }
 </script>
 
